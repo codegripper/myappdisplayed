71f2b858e4eae303e5e0165c80a94957471532e3
Robert Trevino <bobsitestore@yahoo.com>
2025-02-23 21:42:29 -0600

    first files

---
+++ js/tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        maxWidth: {
+          container: "800px",
+        },
+        spacing: {
+          128: "32rem",
+        },
+        colors: {
+          clifford: "#da373d",
+        },
+        fontFamily: {
+          sans: ["ui-sans-serif", "system-ui"],
+        },
+        fontSize: {
+          "main-heading": "64px",
+        },
+        lineHeight: {
+          "extra-loose": "2.5",
+          12: "3rem",
+        },
+        backgroundImage: {
+          "hero-pattern": "url()",
+        },
+      },
+    },
+  };
